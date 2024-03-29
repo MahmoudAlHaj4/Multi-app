13e7603481f3e337cbe61e7dec30c46f7fa744b5
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-03-29 02:45:42 +0200

    add note.jsx

--- note/src/note.jsx
+++ note/src/note.jsx
@@ -1,2 +1,25 @@
 import React from "react";
 import { useState } from "react";
+import "./style.css"
+
+
+const Notes =()=>{
+    return (
+        <div className="notes">
+            <div className="heading">
+                <h1>Notes</h1>
+            </div>
+            <div className="create-note">
+                <div className="note-heading">
+                    <h2>Note</h2>
+                    <button>x</button>
+                </div>
+                
+                <div>
+                    <input type="text" />
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Notes
